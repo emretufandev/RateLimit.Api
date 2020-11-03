20a9f0d5b1d963905cf85e4094e99dd8fe45b978
Emre TUFAN <emretufan@outlook.com.tr>
2020-11-03 23:02:56 +0300

    Add project files.

---
+++ RateLimit.Api/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RateLimit.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Id = 1, Name = "Siemens A++ Buzdolabı", Price = "3500" });
+        }
+    }
+}
